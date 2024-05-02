842c314aa9ff1cabb1c5cfc33f7391164ce53c55
jclaveriepaul <josh.claveriepaul22@my.northampton.ac.uk>
2024-05-02 17:44:52 +0100

    BlissMinds Service Page

---
+++ booking.php
@@ -0,0 +1,35 @@
+<?
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Booking</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <h1>Book an online appointment</h1>
+    <p>Fill in the form below to book an online appointment with us.</p>
+    <div>
+        <h2>Our Services</h2>
+        <hr>
+        <p class="1">Peer Support Network</p>
+        <p class="1">1hr</p> <button class="1" onclick="pspbook.php">Book Now</button>
+        <hr>
+        <p class="2">Individual Therapy</p>
+        <p class="2">1hr</p> <button class="2" onclick="itbook.php">Book Now</button>
+        <hr>
+        <p class="3">Virtual Consultation</p>
+        <p class="3">1hr</p> <button class="3" onclick="vcbook.php">Book Now</button>
+        <hr>
+        <p class="4">Relaxation Techniques</p>
+        <p class="4">1hr</p> <button class="4" onclick="rtbook.php">Book Now</button>
+        <hr>
+    </div>
+</body>
+
+</html>
